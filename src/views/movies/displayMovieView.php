15be2233630b990bcfdb9eeb3f3d775e4b9db917
Axel M. Soares <150051319+AxelMSoares@users.noreply.github.com>
2024-01-22 22:30:55 +0100

    Modifications sur le display des movies

--- src/views/movies/displayMovieView.php
+++ src/views/movies/displayMovieView.php
@@ -8,7 +8,7 @@ get_header('Films', 'admin') ;
 <div class="d-flex justify-content-sm-between flex-wrap">
   <?php foreach ($movies as $movie) { ?>
 
-    <div class="card mb-5" style="width: 28rem;">
+    <div class="card m-5" style="width: 34rem;">
       <img src="..." class="card-img-top" alt="<?= $movie['title'] ?>">
       <div class="card-body">
         <h5 class="card-title"><?= $movie['title'] ?></h5>
@@ -19,9 +19,10 @@ get_header('Films', 'admin') ;
         <li class="list-group-item">Durée: <?= $movie['duration'] ?> min</li>
         <li class="list-group-item">Date de sortie: <?= $movie['release_date'] ?></li>
         <li class="list-group-item">Casting: <?= $movie['casting'] ?></li>
+        <li class="list-group-item">Description: Modifier pour voir</li>
       </ul>
       <div class="card-body">
-        <a href="<?= $router->generate('deleteMovie'); ?><?= $movie['id'] ?>" class="card-link btn btn-danger" onclick="return confirm('Etes-vous sûr de vouloir supprimer ce film?')">Supprimer</a>
+        <a href="<?= $router->generate('deleteMovie'); ?><?= $movie['id'] ?>" class="card-link btn btn-danger">Supprimer</a>
         <a href="<?= $router->generate('editMovie'); ?>/<?= $movie['id'] ?>" class="card-link btn btn-primary">Modifier</a>
       </div>
     </div>
