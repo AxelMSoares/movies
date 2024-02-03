8e08283bf6bf0153dbb887bd2db9e20504f6b2ae
Axel M. Soares <150051319+AxelMSoares@users.noreply.github.com>
2024-02-03 12:23:50 +0100

    Amelioration du responsive pour petits ecrans

--- src/views/movies/admin_displayMovieView.php
+++ src/views/movies/admin_displayMovieView.php
@@ -3,29 +3,33 @@
 get_header('Films', 'admin') ; 
 
 ?>
-
-
+<div>
+  <form class="d-flex" role="search">
+    <input class="form-control me-2" type="search" placeholder="Rechercher un film par son nom" aria-label="Search" name=search>
+    <button class="btn btn-outline-success" type="submit">Rechercher</button>
+  </form>
+</div>
 <div class="d-flex justify-content-sm-between flex-wrap">
   <?php foreach ($movies as $movie) { ?>
-
-    <div class="card m-4" style="width: 16rem;">
-      <img src="/<?= htmlentities($movie['poster']) ?>" class="img-fluid" alt="<?= htmlentities($movie['title']) ?>">
-      <div class="card-body">
-        <h5 class="card-title"><?= htmlentities($movie['title']) ?></h5>
+    <?php if (empty($_GET['search']) || searchByName($movie['title'], $_GET['search'])){ ?>
+      <div class="card m-4" style="width: 16rem;">
+        <img src="/<?= htmlentities($movie['poster']) ?>" class="img-fluid" alt="<?= htmlentities($movie['title']) ?>">
+        <div class="card-body">
+          <h5 class="card-title"><?= htmlentities($movie['title']) ?></h5>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">Réalisateur: <?= htmlentities($movie['director']) ?></li>
+          <li class="list-group-item">Durée: <?= htmlentities($movie['duration']) ?> min</li>
+          <li class="list-group-item">Date de sortie: <?= htmlentities($movie['release_date']) ?></li>
+          <li class="list-group-item">Casting: <?= htmlentities($movie['casting']) ?></li>
+          <li class="list-group-item">Description: Modifier pour voir</li>
+        </ul>
+        <div class="card-body">
+          <a href="<?= $router->generate('deleteMovieConfirm'); ?><?= $movie['id'] ?>" class="card-link btn btn-danger">Supprimer</a>
+          <a href="<?= $router->generate('editMovie'); ?>/<?= $movie['id'] ?>" class="card-link btn btn-primary">Modifier</a>
+        </div>
       </div>
-      <ul class="list-group list-group-flush">
-        <li class="list-group-item">Réalisateur: <?= htmlentities($movie['director']) ?></li>
-        <li class="list-group-item">Durée: <?= htmlentities($movie['duration']) ?> min</li>
-        <li class="list-group-item">Date de sortie: <?= htmlentities($movie['release_date']) ?></li>
-        <li class="list-group-item">Casting: <?= htmlentities($movie['casting']) ?></li>
-        <li class="list-group-item">Description: Modifier pour voir</li>
-      </ul>
-      <div class="card-body">
-        <a href="<?= $router->generate('deleteMovieConfirm'); ?><?= $movie['id'] ?>" class="card-link btn btn-danger">Supprimer</a>
-        <a href="<?= $router->generate('editMovie'); ?>/<?= $movie['id'] ?>" class="card-link btn btn-primary">Modifier</a>
-      </div>
-    </div>
-
+    <?php } ?>
   <?php } ?>
 </div>
 
